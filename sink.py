1fbc6e624091a62299f7a6c88d5936c36a0c18b5
Andreas Ekeroot <andeke@gmail.com>
2014-04-04 15:05:09 +0200

    Added files to .gitignore and now use a context manager on file save.

--- sink.py
+++ sink.py
@@ -6,13 +6,12 @@ class EmlServer(SMTPServer):
     no = 0
     def process_message(self, peer, mailfrom, rcpttos, data):
         filename = '%s-%d.eml' % (datetime.now().strftime('%Y%m%d%H%M%S'), self.no)
-        f = open(filename, 'w')
-        f.write(data)
-        f.close
+        with open(filename, 'w') as fh:
+            fh.write(data)
         print('%s saved.' % filename)
         self.no += 1
 
-def run():
+def main():
     foo = EmlServer(('localhost', 25), None)
     try:
         asyncore.loop()
@@ -21,4 +20,4 @@ def run():
 
 if __name__ == '__main__':
     print("Running!")
-    run()
+    main()
